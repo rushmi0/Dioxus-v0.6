d7dd2c6ea8c5205339d9b581bd2266938cf72f25
lnwza007⚡️ <gcalku@gmail.com>
2025-01-05 21:38:55 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
 use dioxus_app::App;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/src/main.css");
-//const TAILWIND_CSS: Asset = asset!("/public/tailwind.css");
-
 fn main() {
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
     info!("Dioxus v0.6 Starting...");
